b819ef0639e17b92fd83a9330a39cebd47af27ee
Banks Humphreys <iobg@Bankss-MacBook-Pro.local>
2016-07-28 10:31:07 -0500

    used interact with angular successfully

---
+++ app/interactions/noteDragging.js
@@ -0,0 +1,27 @@
+let  x=0,
+	   y=0;
+
+interact('#grid-snap')
+  .draggable({
+    snap: {
+      targets: [
+        interact.createSnapGrid({ x: 30, y: 30 })
+      ],
+      range: Infinity,
+      relativePoints: [ { x: 0, y: 0 } ]
+    },
+    inertia: true,
+    restrict: {
+   		restriction: "parent",
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+      endOnly: true
+    }
+  })
+  .on('dragmove', function (event) {
+    x += event.dx;
+    y += event.dy;
+
+    event.target.style.webkitTransform =
+    event.target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)';
+  });

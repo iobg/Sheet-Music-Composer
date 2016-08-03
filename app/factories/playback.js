5a902c1a731c939f6f06c5bfd08f2352743b14bb
Banks Humphreys <iobg@Bankss-MacBook-Pro.local>
2016-08-03 14:04:02 -0500

    all notes playing on place

--- app/factories/playback.js
+++ app/factories/playback.js
@@ -12,11 +12,11 @@ let playANote = function (noteToPlay) {
 		onsuccess: function() {
 			var delay = 0; // play one note every quarter second
 			var note = noteToPlay; // the MIDI note
-			var velocity = 127; // how hard the note hits
+			var velocity = 255; // how hard the note hits
 			// play the note
 			MIDI.setVolume(0, 127);
 			MIDI.noteOn(0, note, velocity, delay);
-			MIDI.noteOff(0, note, delay + 0.75);
+			MIDI.noteOff(0, note, delay + 1.50);
 		}
 	});
 };
